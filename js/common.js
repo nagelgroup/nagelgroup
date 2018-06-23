7825d9fd7b6450ed77f0ae4354e72e3b49ac48ac
nagelgroup <nagelgroup@yandex.com>
2018-06-23 22:10:44 +0300

    keyup

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 let search = document.querySelector('.search');
 let container = document.querySelector('.container');
 
-search.addEventListener('keypress', event => {
-	container.innerHTML = 'helllo';
+search.addEventListener('keyup', event => {
+	container.innerHTML = search.value;
 });
