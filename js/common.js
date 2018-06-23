137e65f6a18594d7e32e4c3960f94f731f7cbf54
nagelgroup <nagelgroup@yandex.com>
2018-06-24 02:33:19 +0300

    BETA

--- js/common.js
+++ js/common.js
@@ -1,15 +1,46 @@
 let search = document.querySelector('.search');
-let container = document.querySelector('.container li');
+let container = document.querySelector('.container');
+let error = document.querySelector('.error');
 
-let array = 'lubin_pl5 lublin_pl2 teresin_pl2'.split(' ');
+let districts = 'lubin_pl5 lubartow_pl2 lublin_pl2 teresin_pl2'.toUpperCase().split(' ');
 
-let str2 = 'teresin pl2';
+
+console.log(districts)
+
 search.addEventListener('keyup', event => {
-	let str = str2.split(' ');
-	if(search.value === str[0].substring(0, search.value.length) && search.value.length > 0)
-		container.innerHTML = `<b>${search.value}</b>${str[0].substring(search.value.length)} <span>${str[1]}</span>`;
-	else if(search.value.length == 0)
-		container.innerHTML = '';
-	else
-		container.innerHTML = `<i>ERROR</i>`;
+	let _search = search.value.toUpperCase();
+
+	container.innerHTML = '';
+
+	districts.forEach( value => {
+
+		if(_search === value.substring(0, _search.length) && _search.length > 0){
+			let tempArray = value.split('_');
+
+			let li = document.createElement('li');
+			li.innerHTML = `<b>${_search}</b>${tempArray[0].substring(_search.length)}<span class="dist">${tempArray[1]}</span>`;
+
+			container.appendChild(li);
+		} 
+
+		if(container.innerHTML == '' && _search.length > 0){
+			error.style.display = 'block';
+		} else {
+			error.style.display = 'none';
+		}
+
+	});
 });
+
+
+
+	// let str = str2.split(' ');
+
+	// if(search.value === str[0].substring(0, search.value.length) && search.value.length > 0)
+	// 	container.innerHTML = `<b>${search.value}</b>${str[0].substring(search.value.length)} <span>${str[1]}</span>`;
+
+	// else if(search.value.length == 0)
+	// 	container.innerHTML = '';
+
+	// else
+	// 	container.innerHTML = `<i>ERROR</i>`;
